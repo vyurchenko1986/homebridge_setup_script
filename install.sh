9f9852916aec318f171c91e8dcf6c94667532268
Valery Yurchenko <36301615+vyurchenko1986@users.noreply.github.com>
2019-03-19 16:29:06 +0200

    Update install.sh
    Update install.sh 19.03.2019

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo sed -i 's/raspberrypi/homebridge/g' /etc/hostname
+sudo sed -i 's/raspberrypi/homebridge/g' /etc/hosts
+sudo chpasswd <<< "pi:QY32SFyTQfaHDTKd"
+
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs libavahi-compat-libdnssd-dev
 mkdir ~/.homebridge && cp config.json ~/.homebridge
 
 sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
+sudo npm i -g install homebridge-ewelink-max
 
 sudo cp homebridge.service /etc/systemd/system/
 sudo systemctl daemon-reload
